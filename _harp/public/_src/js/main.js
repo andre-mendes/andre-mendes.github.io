83ea81ab5e0aed57f0053402883ef3576e9e04d8
Andre Mendes <mendes.adc@gmail.com>
2016-10-26 17:02:26 -0200

    adding footer

---
+++ _harp/public/_src/js/main.js
@@ -0,0 +1,17 @@
+
+
+
+// $(document).ready(function() {
+// // var home = document.getElementById('home');
+
+//   var movementStrength = 10;
+//   var height = movementStrength / $(window).height();
+//   var width = movementStrength / $(window).width();
+//   $("#home").mousemove(function(e){
+//     var pageX = e.pageX - ($(window).width() / 2);
+//     var pageY = e.pageY - ($(window).height() / 2);
+//     var newvalueX = width * pageX * -1 - 25;
+//     var newvalueY = height * pageY * -1 - 50;
+//     $('#home').css("background-position", newvalueX+"px     "+newvalueY+"px");
+//   });
+// });
