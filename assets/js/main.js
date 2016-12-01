cc09366c26478eaeb6948981a4298ef599e2d35f
AndreMdc <mendes.adc@gmail.com>
2016-12-01 01:11:58 -0200

    'fixing'

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,19 @@ Prism.languages.scss=Prism.languages.extend("css",{comment:{pattern:/(^|[^\\])(?
 // });
 
 
+
+if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || window.location.hostname === 'localhost')) {
+
+  navigator.serviceWorker.register('service-worker.js', {
+    scope: './'
+  }).then(function(registration) {
+    if (typeof registration.update == 'function') {
+      registration.update();
+    }
+  }).catch(function(e) {
+    console.error('Error during service worker registration:', e);
+  });
+
+}
+
+
